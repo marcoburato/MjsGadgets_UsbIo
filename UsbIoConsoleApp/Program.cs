2200a93bb06f3be8289fdda322e7d7f345dff61b
Marco Burato <marco.burato@outlook.com>
2020-03-31 21:20:24 +0200

    Initial commit

---
+++ UsbIoConsoleApp/Program.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Text;
+
+namespace MjsGadgets
+{
+    class Program
+    {
+        private static string _serialNumber;
+
+
+        public static void Main(string[] args)
+        {
+            int i = 0;
+            if (ParseStr(args, 0) == "--serial-number")
+            {
+                _serialNumber = ParseStr(args, 1);
+
+                i = 2;
+            }
+            
+            try
+            {
+                switch (ParseStr(args, i))
+                {
+                    case "list-devices":
+                        ListDevices();
+                        break;
+                    case "get-all-inputs":
+                        GetAllInputs();
+                        break;
+                    case "get-all-relays":
+                        GetAllRelays();
+                        break;
+                    case "get-input":
+                        GetInput(ParseInt(args, i + 1, 1, UsbIo.NumInputs));
+                        break;
+                    case "get-relay":
+                        GetRelay(ParseInt(args, i + 1, 1, UsbIo.NumRelays));
+                        break;
+                    case "set-all-relays":
+                        SetAllRelays(ParseBools(args, i + 1, UsbIo.NumRelays));
+                        break;
+                    case "set-relay":
+                        SetRelay(ParseInt(args, i + 1, 1, UsbIo.NumRelays), ParseBool(args, i + 2));
+                        break;
+                    default:
+                        PrintUsageAndAbort();
+                        break;
+                }
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("Error: " + exc.Message);
+                Environment.Exit(1);
+            }
+        }
+
+        private static string ParseStr(string[] args, int index)
+        {
+            if (index >= args.Length)
+                PrintUsageAndAbort();
+
+            return args[index];
+        }
+
+        private static int ParseInt(string[] args, int index, int min, int max)
+        {
+            if (index >= args.Length)
+                PrintUsageAndAbort();
+
+            if (int.TryParse(args[index], out int value) == false)
+                PrintUsageAndAbort();
+
+            if (value < min || value > max)
+                PrintUsageAndAbort();
+
+            return value;
+        }
+
+        private static bool ParseBool(string[] args, int index)
+        {
+            int i = ParseInt(args, index, 0, 1);
+            return (i != 0);
+        }
+
+        private static bool[] ParseBools(string[] args, int index, int count)
+        {
+            string s = ParseStr(args, index);
+            if (s.Length != count)
+                PrintUsageAndAbort();
+
+            bool[] result = new bool[count];
+            for (int i = 0; i < count; i++)
+            {
+                switch (s[i])
+                {
+                    case '0':
+                        result[i] = false;
+                        break;
+                    case '1':
+                        result[i] = true;
+                        break;
+                    default:
+                        PrintUsageAndAbort();
+                        break;
+                }
+            }
+
+            return result;
+        }
+
+        private static void PrintUsageAndAbort()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("  UsbIoCtrl [--serial-number <serialnumber>] list-devices");
+            Console.WriteLine("  UsbIoCtrl [--serial-number <serialnumber>] get-all-inputs");
+            Console.WriteLine("  UsbIoCtrl [--serial-number <serialnumber>] get-all-relays");
+            Console.WriteLine("  UsbIoCtrl [--serial-number <serialnumber>] get-input <num>");
+            Console.WriteLine("  UsbIoCtrl [--serial-number <serialnumber>] get-relay <num>");
+            Console.WriteLine("  UsbIoCtrl [--serial-number <serialnumber>] set-all-relays <states>");
+            Console.WriteLine("  UsbIoCtrl [--serial-number <serialnumber>] set-relay <num> <state>");
+            Console.WriteLine("where:");
+            Console.WriteLine("  <serialnumber> is the target device serial number. If not provided, the first device found will be used.");
+            Console.WriteLine("  <num> is the input of relay number 1..4");
+            Console.WriteLine("  <state> is 0 (OFF) or 1 (ON)");
+            Console.WriteLine("  <states> sequence of <state>, one for each relay (4)");
+            Environment.Exit(1);
+        }
+
+        private static void ListDevices()
+        {
+            var serialNumbers = UsbIo.GetDevices();
+
+            foreach (var serialNumber in serialNumbers)
+                Console.WriteLine("Device: " + serialNumber);
+        }
+
+        private static void GetAllInputs()
+        {
+            using (var device = Open())
+            {
+                device.GetCurrentStates(out int inputsMask, out _);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < UsbIo.NumInputs; i++)
+                {
+                    bool state = (inputsMask & (1 << i)) != 0;
+                    sb.Append(state ? '1' : '0');
+                }
+                Console.WriteLine(sb.ToString());
+            }
+        }
+
+        private static void GetAllRelays()
+        {
+            using (var device = Open())
+            {
+                device.GetCurrentStates(out _, out int relaysMask);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < UsbIo.NumRelays; i++)
+                {
+                    bool state = (relaysMask & (1 << i)) != 0;
+                    sb.Append(state ? '1' : '0');
+                }
+                Console.WriteLine(sb.ToString());
+            }
+        }
+
+        private static void GetInput(int num)
+        {
+            CheckInputNum(num);
+
+            using (var device = Open())
+            {
+                bool state = device.GetInput(num);
+
+                Console.WriteLine(state ? '1' : '0');
+            }
+        }
+
+        private static void GetRelay(int num)
+        {
+            CheckRelayNum(num);
+
+            using (var device = Open())
+            {
+                bool state = device.GetRelay(num);
+
+                Console.WriteLine(state ? '1' : '0');
+            }
+        }
+
+        private static void SetAllRelays(bool[] states)
+        {
+            int mask = 0;
+            for (int i = 0; i < UsbIo.NumRelays; i++)
+                if (states[i])
+                    mask |= (1 << i);
+
+            using (var device = Open())
+            {
+                device.SetRelays(mask);
+            }
+        }
+
+        private static void SetRelay(int num, bool state)
+        {
+            CheckRelayNum(num);
+
+            using (var device = Open())
+            {
+                device.SetRelay(num, state);
+            }
+        }
+
+        private static void CheckInputNum(int num)
+        {
+            if (num < 1 || num > UsbIo.NumInputs)
+            {
+                Console.WriteLine("Error: invalid input number, must be 1-4");
+                Environment.Exit(1);
+            }
+        }
+
+        private static void CheckRelayNum(int num)
+        {
+            if (num < 1 || num > UsbIo.NumRelays)
+            {
+                Console.WriteLine("Error: invalid relay number, must be 1-4");
+                Environment.Exit(1);
+            }
+        }
+        
+        private static UsbIo Open()
+        {
+            var serialNumbers = UsbIo.GetDevices();
+
+            var serialNumberToOpen = _serialNumber;
+
+            if (serialNumberToOpen == null)
+            {
+                if (serialNumbers.Length == 0)
+                {
+                    Console.WriteLine("No device found");
+                    Environment.Exit(1);
+                }
+
+                serialNumberToOpen = serialNumbers[0];
+
+                if (serialNumbers.Length > 1)
+                    Console.WriteLine($"Warning: multiple devices found, picked first: {serialNumberToOpen}");
+            }
+
+            var device = new UsbIo(serialNumberToOpen);
+
+            return device;
+        }
+    }
+}
---
+++ UsbIoLib/UsbIo.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using HidLibrary;
+
+namespace MjsGadgets
+{
+    public class UsbIo : IDisposable
+    {
+        private const int Vid = 0x0B40;
+        private const int Pid = 0x012D;
+
+        private HidDevice _dev;
+
+        public const int NumInputs = 4;
+        public const int NumRelays = 4;
+
+
+        public UsbIo(string serialNumber)
+        {
+            _dev = GetDeviceBySerialNumber(serialNumber) ?? throw new IOException($"Device with serial number {serialNumber} not found");
+        }
+
+        public void GetCurrentStates(out int inputsMask, out int relaysMask)
+        {
+            byte[] data = new byte[64];
+            data[0] = 0x00;
+            data[1] = 0x9C;
+            data[2] = 0x03;
+            data[3] = 0x00;
+            data[4] = 0xF0;
+            if (_dev.Write(data) == false)
+                throw new IOException("HID Write failed");
+
+            var response = _dev.Read();
+            if (response.Status != HidDeviceData.ReadStatus.Success)
+                throw new IOException("HID Read failed");
+
+            byte mask = response.Data[3];
+            inputsMask = (byte)((mask >> 4) ^ 0x0F);
+            relaysMask = (byte)(mask & 0x0F);
+        }
+
+        public bool GetInput(int num)
+        {
+            if (num < 1 || num > NumInputs)
+                throw new ArgumentOutOfRangeException(nameof(num), $"Input number must be between 1 and {NumInputs}");
+
+            GetCurrentStates(out int inputsMask, out _);
+            return (inputsMask & (1 << (num - 1))) != 0;
+        }
+
+        public bool GetRelay(int num)
+        {
+            if (num < 1 || num > NumRelays)
+                throw new ArgumentOutOfRangeException(nameof(num), $"Relay number must be between 1 and {NumRelays}");
+
+            GetCurrentStates(out _, out int relaysMask);
+            return (relaysMask & (1 << (num - 1))) != 0;
+        }
+
+        public void SetRelay(int num, bool state)
+        {
+            if (num < 1 || num > NumRelays)
+                throw new ArgumentOutOfRangeException(nameof(num), $"Relay number must be between 1 and {NumRelays}");
+
+            byte[] data = new byte[64];
+            data[0] = 0x00;
+            data[1] = 0x9F;
+            data[2] = 0x03;
+            data[3] = (byte)(num - 1);
+            data[4] = (byte)(state ? 1 : 0);
+            if (_dev.Write(data) == false)
+                throw new IOException("HID Write failed");
+            _dev.Read();
+        }
+
+        public void SetRelays(int mask)
+        {
+            if (mask > 0x0F)
+                throw new ArgumentOutOfRangeException(nameof(mask), $"Only {NumRelays} bits expected");
+
+            byte[] data = new byte[64];
+            data[0] = 0x00;
+            data[1] = 0x9D;
+            data[2] = 0x03;
+            data[3] = (byte)mask;
+            data[4] = 0;
+            if (_dev.Write(data) == false)
+                throw new IOException("Write failed");
+            _dev.Read();
+        }
+
+        public static string[] GetDevices()
+        {
+            var list = new List<string>();
+
+            var devices = HidDevices.Enumerate(Vid, Pid);
+            foreach (var device in devices)
+                if (device.ReadSerialNumber(out byte[] data))
+                    list.Add(DecodeSerialNumberData(data));
+
+            return list.ToArray();
+        }
+
+        static private HidDevice GetDeviceBySerialNumber(string serialNumber)
+        {
+            var devices = HidDevices.Enumerate(Vid, Pid);
+            foreach (var device in devices)
+                if (device.ReadSerialNumber(out byte[] data) && DecodeSerialNumberData(data) == serialNumber)
+                    return device;
+            return null;
+        }
+
+        static private string DecodeSerialNumberData(byte[] data)
+        {
+            var s = Encoding.Unicode.GetString(data);
+            var terminatorIndex = s.IndexOf('\0');
+            if (terminatorIndex >= 0)
+                return s.Substring(0, terminatorIndex);
+            else
+                return s;
+        }
+
+        #region IDisposable Support
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    _dev.Dispose();
+                }
+
+                disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
